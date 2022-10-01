bec62af8d789921b4ab25e11cdb1b51435a659fd
edvin <edvin@OMNISSIAH>
2022-10-01 19:52:50 +0200

    Add project files.

---
+++ Menu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämmningsuppgift
+{
+    public enum MainMenuChoice
+    {
+        Shirts = 1,
+        Mugs,
+        Exit
+    }
+
+    public enum ShirtMenu
+    {
+        Generate = 1,
+        Show,
+        Exit
+    }
+
+    public class Menu
+    {
+        public MainMenuChoice PrintMainMenu()
+        {
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("1: Shirts");
+            Console.WriteLine("2: Mugs");
+            Console.WriteLine("3: Exit");
+
+            if (!int.TryParse(Console.ReadLine(), out int mainMenuChoice))
+            {
+                Console.WriteLine("Endast siffror!");
+            }
+
+            return (MainMenuChoice)mainMenuChoice;
+        }
+
+        public ShirtMenu PrintShirtMenu()
+        {
+            Console.WriteLine("1: Generate shirts");
+            Console.WriteLine("2: Show shirts");
+            Console.WriteLine("3: Exit");
+
+            if (!int.TryParse(Console.ReadLine(), out int shirtMenuChoice))
+            {
+                Console.WriteLine("Endast siffror!");
+            }
+
+            return (ShirtMenu)shirtMenuChoice;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Inlämmningsuppgift
+{
+    internal class Program
+    {
+        private static bool _mainMenuRunning = true;
+        private static bool _shirtMenuRunning = true;
+
+        private static void Main(string[] args)
+        {
+            var menu = new Menu();
+
+            while (_mainMenuRunning == true)
+            {
+                switch (menu.PrintMainMenu())
+                {
+                    case MainMenuChoice.Shirts:
+                        {
+                            while (_shirtMenuRunning == true)
+                            {
+                                switch (menu.PrintShirtMenu())
+                                {
+                                    case ShirtMenu.Generate:
+                                        {
+
+                                        }
+                                        break;
+                                    case ShirtMenu.Show:
+                                        {
+
+                                        }
+                                        break;
+                                    case ShirtMenu.Exit:
+                                        {
+                                            _shirtMenuRunning = false;
+                                        }
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                            break;
+                        }
+                    case MainMenuChoice.Mugs:
+                        {
+                            break;
+                        }
+                    case MainMenuChoice.Exit:
+                        {
+                            _mainMenuRunning = false;
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
+
+        }
+    }
+}
